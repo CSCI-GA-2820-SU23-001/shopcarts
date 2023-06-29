4a3b21a0f03a9d800c0bf1bcc04b0ccf625a645e
Kangle <kx2136@nyu.edu>
2023-06-29 16:27:15 +0000

    initial version of Get-A-Shopcart-Item

--- service/routes.py
+++ service/routes.py
@@ -29,3 +29,29 @@ def index():
 ######################################################################
 
 # Place your REST API code here ...
+
+# RETRIEVE A ITEM FROM SHOPCART
+######################################################################
+@app.route("/shopcarts/<int:customer_id>/items/<int:item_id>", methods=["GET"])
+def get_items(customer_id, item_id):
+    """
+    Get a Item
+
+    This endpoint returns just a item
+    """
+    app.logger.info(
+        "Request to retrieve Item %s for Shopcart id: %s", (item_id, customer_id)
+    )
+
+    # See if the item exists and abort if it doesn't
+    item = Item.find(item_id)
+    if not item:
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Shopcart with id '{item_id}' could not be found.",
+        )
+
+    app.logger.info("Returning item: %s", item.id)
+    return make_response(jsonify(item.serialize()), status.HTTP_200_OK)
+
+

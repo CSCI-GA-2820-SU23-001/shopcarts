a0cec815cd0315aa3fb5a7242f983bfa3fe2a558
Yuzhao Luo <yl10756@nyu.edu>
2023-07-30 14:18:03 -0400

    Implement of 6 Shopcarts APIs

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -6,11 +6,17 @@ Steps file for web interactions with Selenium
 For information on Waiting until elements are present in the HTML see:
     https://selenium-python.readthedocs.io/waits.html
 """
+
+import logging
 from behave import when, then
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions
 
 # uncomment if we use 'shopcart_' as id prefix in the index page
 # ID_PREFIX = 'shopcart_'
+SHOPCART_PREFIX = 'shopcart_'
+ITEM_PREFIX = 'item_'
 
 
 @when('I visit the "home page"')
@@ -31,3 +37,127 @@ def step_impl(context, message):  # pylint: disable=E223
 def step_impl(context, text_string):
     element = context.driver.find_element(By.TAG_NAME, 'body')
     assert (text_string not in element.text)
+
+@when('I visit the "{page}" api page')
+def step_impl(context, page):
+    if page == "Shopcart":
+        context.driver.get(context.shopcart_url)
+    else:
+        context.driver.get(context.item_url)
+
+@when('I press the "{button}" button in "{page}" page')
+def step_impl(context, button, page):
+    # button_id = 'search-shopcart-btn'
+    button_id = button.lower() + '-' + page.lower() + '-btn'
+    context.driver.find_element(By.ID, button_id).click()
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    assert(found)
+
+@then('I should see "{name}" in the results in "{page}" page')
+def step_impl(context, name, page):
+    if page == "Shopcart":
+        search_results = 'search_shopcarts_results'
+    else:
+        search_results = 'search_items_results'
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, search_results),
+            name
+        )
+    )
+    assert(found)
+
+@then('I should not see "{name}" in the results in "{page}" page')
+def step_impl(context, name, page):
+    if page == "Shopcart":
+        search_results = 'search_shopcarts_results'
+    else:
+        search_results = 'search_items_results'
+    element = context.driver.find_element(By.ID, search_results)
+    assert(name not in element.text)
+
+@when('I set the "{element_name}" to "{text_string}" in "{page}" page')
+def step_impl(context, element_name, text_string, page):
+    if page == "Shopcart":
+        ID_PREFIX = SHOPCART_PREFIX
+    else:
+        ID_PREFIX = ITEM_PREFIX
+    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
+    element = context.driver.find_element(By.ID, element_id)
+    element.clear()
+    element.send_keys(text_string)
+
+@then('the "{element_name}" field should be empty in "{page}" page')
+def step_impl(context, element_name, page):
+    if page == "Shopcart":
+        ID_PREFIX = SHOPCART_PREFIX
+    else:
+        ID_PREFIX = ITEM_PREFIX
+    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
+    element = context.driver.find_element(By.ID, element_id)
+    assert(element.get_attribute('value') == u'')
+
+##################################################################
+# These two function simulate copy and paste
+##################################################################
+@when('I copy the "{element_name}" field in "{page}" page')
+def step_impl(context, element_name, page):
+    if page == "Shopcart":
+        ID_PREFIX = SHOPCART_PREFIX
+    else:
+        ID_PREFIX = ITEM_PREFIX
+    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
+    element = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.presence_of_element_located((By.ID, element_id))
+    )
+    context.clipboard = element.get_attribute('value')
+    logging.info('Clipboard contains: %s', context.clipboard)
+
+@when('I paste the "{element_name}" field in "{page}" page')
+def step_impl(context, element_name, page):
+    if page == "Shopcart":
+        ID_PREFIX = SHOPCART_PREFIX
+    else:
+        ID_PREFIX = ITEM_PREFIX
+    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
+    element = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.presence_of_element_located((By.ID, element_id))
+    )
+    element.clear()
+    element.send_keys(context.clipboard)
+
+@then('I should see "{text_string}" in the "{element_name}" field in "{page}" page')
+def step_impl(context, text_string, element_name, page):
+    if page == "Shopcart":
+        ID_PREFIX = SHOPCART_PREFIX
+    else:
+        ID_PREFIX = ITEM_PREFIX
+    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element_value(
+            (By.ID, element_id),
+            text_string
+        )
+    )
+    assert(found)
+
+@when('I change "{element_name}" to "{text_string}" in "{page}" page')
+def step_impl(context, element_name, text_string, page):
+    if page == "Shopcart":
+        ID_PREFIX = SHOPCART_PREFIX
+    else:
+        ID_PREFIX = ITEM_PREFIX
+    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
+    element = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.presence_of_element_located((By.ID, element_id))
+    )
+    element.clear()
+    element.send_keys(text_string)
---
+++ service/static/js/rest_api.js
@@ -0,0 +1,249 @@
+$(function () {
+
+    function update_form_shopcart(res) {
+        $("#shopcart_id").val(res.id);
+        $("#shopcart_name").val(res.name);
+    }
+
+    function clear_form_shopcart() {
+        $("#shopcart_id").val("");
+        $("#shopcart_name").val("");
+    }
+
+    function flash_message(message) {
+        $("#flash_message").empty();
+        $("#flash_message").append(message);
+    }
+
+    // ****************************************
+    // Create a Shopcart
+    // ****************************************
+
+    $("#create-shopcart-btn").click(function () {
+
+        let name = $("#shopcart_name").val();
+
+        let data = {
+            "name": name
+        };
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "POST",
+            url: "/shopcarts",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+        });
+
+        ajax.done(function(res){
+            update_form_shopcart(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Update a Pet
+    // ****************************************
+
+    $("#update-shopcart-btn").click(function() {
+
+        let id = $("#shopcart_id").val();
+        let name = $("#shopcart_name").val();
+
+        let data = {
+            "name": name
+        };
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+                type: "PUT",
+                url: `/shopcarts/${id}`,
+                contentType: "application/json",
+                data: JSON.stringify(data)
+            })
+
+        ajax.done(function(res){
+            update_form_shopcart(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Retrieve a Shopcart
+    // ****************************************
+
+    $("#retrieve-shopcart-btn").click(function () {
+
+        let shopcart_id = $("#shopcart_id").val();
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "GET",
+            url: `/shopcarts/${shopcart_id}`,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax.done(function(res){
+            update_form_shopcart(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            clear_form_shopcart()
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Delete a Shopcart
+    // ****************************************
+
+    $("#delete-shopcart-btn").click(function () {
+
+        let id = $("#shopcart_id").val();
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "DELETE",
+            url: `/shopcarts/${id}`,
+            contentType: "application/json",
+            data: '',
+        })
+
+        ajax.done(function(res){
+            clear_form_shopcart()
+            flash_message("Shopcart has been Deleted!")
+        });
+
+        ajax.fail(function(res){
+            flash_message("Server error!")
+        });
+
+    });
+
+    $("#clear-shopcart-btn").click(function () {
+        $("#shopcart_id").val("");
+        $("#flash_message").empty();
+        clear_form_shopcart()
+    });
+
+    $("#checkout-shopcart-btn").click(function () {
+
+        let name = $("#shopcart_name").val()
+
+        $("#flash_message").empty();
+
+        let ajax_list = $.ajax({
+            type: "GET",
+            url: `/shopcarts`,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax_list.done(function(res){
+            for (let i=0; i < res.length; i++) {
+                let shopcart = res[i];
+                shopcart_id = shopcart.id;
+                items = shopcart['items'];
+                if (items.length != 0) {
+                    for (let j=0; j < items.length; j++) {
+                        item_id = shopcart.items[j].id;
+                        $.ajax({
+                            type: "DELETE",
+                            url: `/shopcarts/${shopcart_id}/items/${item_id}`,
+                            contentType: "application/json",
+                            data: '',
+                        })
+                    }
+                }
+
+            }
+        });
+
+        ajax_list.done(function(res){
+            clear_form_shopcart()
+            flash_message("Checkout all shopcarts!")
+        });
+
+        ajax_list.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+    });
+
+    $("#search-shopcart-btn").click(function () {
+
+        let name = $("#shopcart_name").val();
+
+        let queryString = ""
+        if (name) {
+            queryString += 'name=' + name
+        }
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "GET",
+            url: `/shopcarts?${queryString}`,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax.done(function(res){
+            $("#search_shopcarts_results").empty();
+            let table = '<table class="table table-striped" cellpadding="10">'
+            table += '<thead><tr>'
+            table += '<th class="col-md-1">ID</th>'
+            table += '<th class="col-md-3">Name</th>'
+            table += '<th class="col-md-1">Item_ID</th>'
+            table += '<th class="col-md-3">Item_Name</th>'
+            table += '<th class="col-md-2">Quantity</th>'
+            table += '<th class="col-md-2">Price</th>'
+            table += '</tr></thead><tbody>'
+            let firstShopcart = "";
+            for (let i = 0; i < res.length; i++) {
+                let shopcart = res[i];
+                items = shopcart['items'];
+                if (items.length != 0) {
+                    for (let j = 0; j < items.length; j++) {
+                        table += `<tr id="row_${i}"><td>${shopcart.id}</td><td>${shopcart.name}</td><td>${shopcart.items[j]['id']}</td><td>${shopcart.items[j]['name']}</td><td>${shopcart.items[j]['quantity']}</td><td>${shopcart.items[j]['price']}</td></tr>`
+                    }
+                } else {
+                    table += `<tr id="row_${i}"><td>${shopcart.id}</td><td>${shopcart.name}</td><td></td><td></td><td></td><td></td></tr>`
+                }
+                
+                if (i == 0) {
+                    firstShopcart = shopcart;
+                }
+            }
+            table += '</tbody></table>';
+            $("#search_shopcarts_results").append(table);
+
+            if (firstShopcart != "") {
+                update_form_shopcart(firstShopcart)
+            }
+
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+})

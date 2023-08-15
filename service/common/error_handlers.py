c240cc88fa06d78af7d915a28ced0dacafcdf60f
Wan-Yu Lin <wl2484@nyu.edu>
2023-08-15 11:57:08 -0400

    :fire: remove unused code in error_handler.py

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -43,14 +43,3 @@ def bad_request(error):
         ),
         status.HTTP_400_BAD_REQUEST,
     )
-
-
-@app.errorhandler(status.HTTP_404_NOT_FOUND)
-def not_found(error):
-    """Handles resources not found with 404_NOT_FOUND"""
-    message = str(error)
-    app.logger.warning(message)
-    return (
-        jsonify(status=status.HTTP_404_NOT_FOUND, error="Not Found", message=message),
-        status.HTTP_404_NOT_FOUND,
-    )

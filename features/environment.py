80718ac637f04b6095071b1f2067511ed967e260
LillisLuo <113263069+LillisLuo@users.noreply.github.com>
2023-08-15 03:29:58 -0400

    Update environment.py

--- features/environment.py
+++ features/environment.py
@@ -5,7 +5,7 @@ from os import getenv
 from selenium import webdriver
 
 WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
-BASE_URL = getenv('BASE_URL', 'http://localhost:8000')
+BASE_URL = getenv('BASE_URL', 'http://localhost:8000/api')
 SHOPCART_URL = getenv('SHOPCART_URL', 'http://localhost:8000/shopcartsapi')
 ITEM_URL = getenv('ITEM_URL', 'http://localhost:8000/itemsapi')
 DRIVER = getenv('DRIVER', 'chrome').lower()

c3494fcecd5717a43dd4ee9b084d595e9f21de25
Wan-Yu Lin <117547590+wl2484@users.noreply.github.com>
2023-08-15 17:46:37 -0400

    Revert "Set pool_recycle (maximum number of seconds a connection can persist)"

--- service/config.py
+++ service/config.py
@@ -15,7 +15,6 @@ DATABASE_URI = os.getenv(
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_POOL_SIZE = 2
 
 # Secret for session management
 SECRET_KEY = os.getenv("SECRET_KEY", "s3cr3t-key-shhhh")
